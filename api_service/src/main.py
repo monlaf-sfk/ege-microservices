885ef9ad5618f66ece53e9f7761e65cf52ab7e58
monleylafmoy <monleylafmoy@gmail.com>
2025-11-26 21:42:21 +0500

    feat: add logging api_service

--- api_service/src/main.py
+++ api_service/src/main.py
@@ -1,10 +1,35 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI , Request
 from tortoise.contrib.fastapi import register_tortoise
 from api_service.src.config import settings
 from api_service.src.routes import users, scores
+from loguru import logger
+import sys
 
-
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    level="INFO"
+)
 app = FastAPI(title="EGE Scoring API")
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+
+    logger.info(f"Incoming Request: {request.method} {request.url.path}")
+
+    try:
+        response = await call_next(request)
+
+        process_time = (time.time() - start_time) * 1000
+        logger.info(f"Completed: {response.status_code} | Time: {process_time:.2f}ms")
+        return response
+    except Exception as e:
+        logger.exception("Request failed")
+        raise e
 
 app.include_router(users.router, prefix="/api/v1")
 app.include_router(scores.router, prefix="/api/v1")
@@ -19,4 +44,5 @@ register_tortoise(
 
 @app.get("/health")
 async def health_check():
+    logger.debug("Health check called")
     return {"status": "ok", "db": "connected"}
